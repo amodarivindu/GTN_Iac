d2b28344c7823690848dbca810e42175edcb84e8
Amoda Dharmasiri <amoda.dharmasiri@globalwavenet.com>
2025-04-07 07:41:27 +0530

    change main tf file

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = ">= 1.0.0"
+  
+  backend "local" {}
+}
+
+required_providers {
+  aws = {
+    source  = "hashicorp/aws"
+    version = "~> 5.0.0"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
